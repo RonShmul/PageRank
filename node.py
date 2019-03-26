a065f95a5df06a7624c5f5b05aac32b59cd64876
Ronshmul <ron.s@nano-di.com>
2019-03-26 22:05:21 +0200

    only calculate page ranks left

--- node.py
+++ node.py
@@ -2,7 +2,9 @@ class Node:
     def __init__(self, name):
         self.name = name
         self.list_of_in = []
+        self.list_of_out = []
         self.page_rank = 0
+        self.temp_pr = 0
 
     def update_page_rank(self, n, beta):
         pass
--- page_rank.py
+++ page_rank.py
@@ -1,26 +1,89 @@
-import pandas as pd
-
+import csv
+from node import Node
+graph = {}
+mapper = {}
+page_rank_list = []
 
 def load_graph(path):
-    df = pd.read_csv(path)
-    df.tostring()  # check if needs?
-    # needs to think about the best way to save the edges- maybe a dictionary: for each node the list of nodes which have a connection to it
+    """
+    loads csv file and creates 2 dictionaries: mapper which maps node name to node id
+    and graph which has the node id as key and Node object as value
+    the Node object has a list_of_out which contains all of its destinations (as source node)
+    and a list_of_in with all of its sources (as destination node)
+    :param path: string
+    :return: None
+    """
+    global graph
+    global mapper
+    index = 1  # index for id allocation
+    with open(path, 'rb') as file:
+        reader = csv.reader(file, delimiter=',')
+    for row in reader:
+        src = str(row[0])
+        dest = str(row[1])
+
+        # insert to mapper if not exists and get the source id and destination id
+        if src in mapper:
+            src_id = mapper[src]
+        else:
+            src_id = index
+            mapper[src] = index
+            index += 1
+
+        if dest in mapper:
+            dest_id = mapper[dest]
+        else:
+            dest_id = index
+            mapper[dest] = index
+            index += 1
+
+        # insert Node object if not exists and insert source id to list_of_in of destination node
+        # and insert destination id to list_of_out of source node
+        src_node = graph.get(src_id, Node(src))
+        graph[src_id] = src_node
+
+        dest_node = graph.get(dest_id, Node(dest))
+        graph[dest_id] = dest_node
+
+        src_node.list_of_out.append(dest_id)
+        dest_node.list_of_in.append(src_id)
 
 
-def calculate_page_rank():
+def calculate_page_rank(b=0.85, e=0.001):
     pass
 
 
 def get_PageRank(node_name):
-    pass
+    """
+    get node name and return its pageRank.
+    If the name node doesn't exist return -1
+    :param node_name: string
+    :return pageRank: float
+    """
+    node_obj = graph.get(mapper.get(node_name, -1), -1)
+    if node_obj == -1:
+        return -1
+    else:
+        return node_obj.page_rank
 
 
 def Get_top_nodes(n):
-    pass
+    """
+    Return a list of n nodes sorted by pageRank from high to low.
+    The list contains tuples (node name, pageRank value)
+    :param n: integer
+    :return page_rank_list: list<node_name, pageRank>
+    """
+    return page_rank_list[:int(n)]
 
 
 def get_all_PageRank():
-    pass
+    """
+    Return a list of all nodes sorted by pageRank from high to low.
+    The list contains tuples (node name, pageRank value)
+    :return page_rank_list: list<node_name, pageRank>
+    """
+    return page_rank_list
 
 
 """
