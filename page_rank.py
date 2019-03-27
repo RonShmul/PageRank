65e3def710b48a51f0c0cb5bc92240cd8a8f10e5
Ronshmul <ron.s@nano-di.com>
2019-03-27 22:19:16 +0200

    finish calculate page rank

--- page_rank.py
+++ page_rank.py
@@ -17,37 +17,37 @@ def load_graph(path):
     global graph
     global mapper
     index = 1  # index for id allocation
-    with open(path, 'rb') as file:
+    with open(path) as file:
         reader = csv.reader(file, delimiter=',')
-    for row in reader:
-        src = str(row[0])
-        dest = str(row[1])
+        for row in reader:
+            src = str(row[0])
+            dest = str(row[1])
 
-        # insert to mapper if not exists and get the source id and destination id
-        if src in mapper:
-            src_id = mapper[src]
-        else:
-            src_id = index
-            mapper[src] = index
-            index += 1
+            # insert to mapper if not exists and get the source id and destination id
+            if src in mapper:
+                src_id = mapper[src]
+            else:
+                src_id = index
+                mapper[src] = index
+                index += 1
 
-        if dest in mapper:
-            dest_id = mapper[dest]
-        else:
-            dest_id = index
-            mapper[dest] = index
-            index += 1
+            if dest in mapper:
+                dest_id = mapper[dest]
+            else:
+                dest_id = index
+                mapper[dest] = index
+                index += 1
 
-        # insert Node object if not exists and insert source id to list_of_in of destination node
-        # and insert destination id to list_of_out of source node
-        src_node = graph.get(src_id, Node(src))
-        graph[src_id] = src_node
+            # insert Node object if not exists and insert source id to list_of_in of destination node
+            # and insert destination id to list_of_out of source node
+            src_node = graph.get(src_id, Node(src))
+            graph[src_id] = src_node
 
-        dest_node = graph.get(dest_id, Node(dest))
-        graph[dest_id] = dest_node
+            dest_node = graph.get(dest_id, Node(dest))
+            graph[dest_id] = dest_node
 
-        src_node.list_of_out.append(dest_id)
-        dest_node.list_of_in.append(src_id)
+            src_node.list_of_out.append(dest_id)
+            dest_node.list_of_in.append(src_id)
 
 
 def initial_pr():
@@ -57,14 +57,18 @@ def initial_pr():
 
 
 def calculate_page_rank(b=0.85, e=0.001):
+    global page_rank_list
     initial_pr()
     pr_sum = 0
     iter_num = 0
-    while iter_num < 20 and pr_sum > e:
+    first = True
+    while (iter_num < 20 and pr_sum > e) or first:
+        first = False
         calculate_ranks(b)
         for (key, value) in graph.items():
             pr_sum += abs(value.page_rank - value.prev_pr)
         iter_num += 1
+    page_rank_list = sorted(graph.items(), key=lambda kv: kv[1].page_rank, reverse=True)
 
 
 def calculate_ranks(b):
@@ -72,7 +76,11 @@ def calculate_ranks(b):
     for (key, value) in graph.items():
         # update previous page rank- move all current to prev
         value.prev_pr = value.page_rank
-        value.page_rank = value.calculate_page_rank(b)
+        temp_rank = 0
+        for node_id in value.list_of_in:
+            node = graph[node_id]
+            temp_rank += b * (node.prev_pr / len(node.list_of_out))
+        value.page_rank = temp_rank
         S += value.page_rank
     factor = (1-S)/len(graph)
     for (key, value) in graph.items():
@@ -112,10 +120,9 @@ def get_all_PageRank():
     return page_rank_list
 
 
-"""
-from bisect import bisect
-data = []
-for x in range(0,len(df[0])):
-    a,b = randint(1,10),randint(1,100)
-    data.insert(bisect(data,(a,1000)),(a,b))
-"""
+p = 'Wikipedia_votes.csv'
+load_graph(p)
+calculate_page_rank()
+# print(graph)
+# print(get_all_PageRank())
+top = Get_top_nodes(4)
