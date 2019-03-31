6609bbed845c2f2a0c9e68e786fe7128f8be7ea4
Sivan Rejensky <>
2019-03-31 09:48:21 +0300

    node done

--- node.py
+++ node.py
@@ -5,13 +5,3 @@ class Node:
         self.list_of_out = []
         self.page_rank = 0
         self.prev_pr = 0
-
-    def update_page_rank(self, n, beta):
-        pass
-
-    def calculate_page_rank(self, b):
-        temp_rank = 0
-        if len(self.list_of_in) > 0:
-            for node in self.list_of_in:
-                temp_rank += b * (node.prev_pr / len(node.list_of_out))
-        return temp_rank
